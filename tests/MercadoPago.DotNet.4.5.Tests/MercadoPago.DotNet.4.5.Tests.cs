0f4e4d2b708d114ab883f879c72cc2f18cbd8e47
Federico Berasategui <una_oportunidad_de_ser@hotmail.com>
2014-11-05 17:15:12 -0300

    Added NUnit Nuget Package and created Unit Tests Project

---
+++ tests/MercadoPago.DotNet.4.5.Tests/MercadoPago.DotNet.4.5.Tests.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MercadoPago.DotNet.Tests
+{
+    [TestFixture]
+    public class MPTests
+    {
+        
+    }
+}
