f4f21ff0fd005dc6abe318fa23d3bd00cc39049a
jesujcastillom <jesuj.castillom@hotmail.com>
2016-04-07 14:05:23 -0430

    Create Currency.cs
    Currency dictionary for every available country in mercadopago.

---
+++ src/MercadoPago.DotNet.4.5/Model/Currency.cs
@@ -0,0 +1,24 @@
+namespace MercadoPago.DotNet.Model
+{
+    public class Currency
+    {
+        public const string Argentina = "ARS";
+        public const string Bolivia = "BOB";
+        public const string Brazil = "BRL";
+        public const string Chile = "CLP";
+        public const string CostaRica = "CRC";
+        public const string Cuba = "CUC";
+        public const string Dolar = "USD";
+        public const string DominicanRepublic = "DOP";
+        public const string Euro = "EUR";
+        public const string Guatemala = "GTQ";
+        public const string Honduras = "HNL";
+        public const string Mexico = "MXN";
+        public const string Nicaragua = "NIO";
+        public const string Panama = "PAB";
+        public const string Paraguay = "PYG";
+        public const string Peru = "PEN";
+        public const string Venezuela = "VEF";
+
+    }
+}
